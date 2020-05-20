9f01fd0fa800ecfd1158ed102016af5afb920011
Pablo Gonzalez <pabgon0@gmail.com>
2020-05-20 01:35:23 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20200520013317) do
 
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "ingredients"
+    t.string "cook_time"
+  end
+
 end
